a23d503f25b56451e2bac63f7d084822abacfdab
phusitdontree <phusit.dontree@gmail.com>
2019-01-24 14:44:12 +0700

    2401621444

---
+++ dbconnect.php
@@ -0,0 +1,17 @@
+<?php 
+
+include ("configs/connectstring.php");
+//mysql_connect ($host,$user,$pass) or die ("ติดต่อเครื่อง Database Server ไม่สำเร็จ");
+//mysql_select_db($db) or die ("ติดต่อฐานข้อมูลไม่สำเร็จ");
+//mysql_query("SET NAMES utf8")
+// Create connection
+$conn = new mysqli($host, $user, $pass,$db);
+$conn->query("SET NAMES UTF8");
+//$conn->query("SET NAMES TIS-620");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//else echo "connected";
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,36 @@
+<html>
+<head>
+<meta charset="utf8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
 <?php
+
 // Database Connection
-	include dbconnect.php;
+	include("dbconnect.php");
+
 // Autentication
 // Authorization
 // Show Menu
 // Show Content
+
+	$result=$conn->query("select * from KPI");
+	if ($conn->connect_error) {
+    	die("Connect Error (" . $conn->connect_errno . ") ". $conn->connect_error);
+	}
+
+	echo 'Success... ' . $conn->host_info . "\n";
+	$fields = $result->fetch_fields();
+	
+	
+	while($row=$result->fetch_assoc()){
+		foreach($fields as $fvalue){
+			  printf("%s : %s <br>",$fvalue->name,$row[$fvalue->name]);
+		}
+		echo "<hr>";
+	}
+	$result->free();
+	$conn->close();
 ?>
+
+</body>
