1001ce8cc18f47196af9aa42f9051ff15240218e
phusitdontree <phusit.dontree@gmail.com>
2019-01-24 16:43:17 +0700

    2401621643

--- index.php
+++ index.php
@@ -5,30 +5,50 @@
 </head>
 <body>
 <?php
-
+	$authenticated=0;
 // Database Connection
 	include("dbconnect.php");
-
 // Autentication
+	// By User & Password
+
+	$user=$_POST["muser"];
+	$pass=md5($_POST["mpass"]);
+	$skey=$_POST["skey"];
+	$thisuserid=$_POST["thisuserid"];
+	$res = $conn->query("select * from users where username='$user' and password='$pass'");
+	if($res->num_rows>0){
+		$row = $res->fetch_assoc();
+ 		$thisuserid=$row['uid'];
+ 		$thisgroupid=$row['gid'];
+		$thisusername=$row["username"];
+		session_start();
+		$skey=md5(date("Ymd"),$pass);
+		$_SESSION[$thisuserid]=$skey;
+ 		session_write_close();
+	}
+
+	// By Session ID
 // Authorization
 // Show Menu
 // Show Content
-
-	$result=$conn->query("select * from KPI");
-	if ($conn->connect_error) {
-    	die("Connect Error (" . $conn->connect_errno . ") ". $conn->connect_error);
-	}
-
-	echo 'Success... ' . $conn->host_info . "\n";
-	$fields = $result->fetch_fields();
-	
-	
-	while($row=$result->fetch_assoc()){
-		foreach($fields as $fvalue){
-			  printf("%s : %s <br>",$fvalue->name,$row[$fvalue->name]);
+	if($skey==$_SESSION[$thisuserid]){
+		$result=$conn->query("select * from KPI");
+		if ($conn->connect_error) {
+    		die("Connect Error (" . $conn->connect_errno . ") ". $conn->connect_error);
 		}
-		echo "<hr>";
+
+		echo 'Success... ' . $conn->host_info . "\n";
+		$fields = $result->fetch_fields();
+		while($row=$result->fetch_assoc()){
+			foreach($fields as $fvalue){
+				  printf("%s : %s <br>",$fvalue->name,$row[$fvalue->name]);
+			}
+			echo "<hr>";
+		}
 	}
+	else{
+		include("loginform.php");
+	}	
 	$result->free();
 	$conn->close();
 ?>
---
+++ loginform.php
@@ -0,0 +1,14 @@
+		<form method="post" action="index.php">
+		<table style="position:absolute; top:50%; left:50%; margin:-100px 0px 0px -150px; ">
+		<tr><th colspan="2">Web Information System Portal</th></tr>
+		<tr>
+		<td>username (ชื่อผู้ใช้)</td>
+		<td><input type="text" size="10" name="muser"/></td>
+		</tr>
+		<tr>
+		<td>password (รหัสผ่าน)</td>	
+		<td><input type="password" size="10" name="mpass"></td>
+		</tr>
+		<tr><td colspan="2"><center><input type="submit" value="Sign In"/> 
+		<input type="submit" value="Sign Up"/></center></td></tr>
+		</form>
